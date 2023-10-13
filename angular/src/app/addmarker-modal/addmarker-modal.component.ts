fdecae1d0e9e9b9274ed097a15730c560214ec90
Ekim <ekimbasoglu@hotmail.com>
2023-10-13 14:18:16 +0200

    Addmarker inited

--- angular/src/app/addmarker-modal/addmarker-modal.component.ts
+++ angular/src/app/addmarker-modal/addmarker-modal.component.ts
@@ -3,11 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-addmarker-modal',
   templateUrl: './addmarker-modal.component.html',
-  styleUrls: ['./addmarker-modal.component.css']
+  styleUrls: ['./addmarker-modal.component.css'],
 })
 export class AddmarkerModalComponent {
+  formObj: any = {
+    name: '',
+    address: '',
+    startingPrice: '',
+    lng: 0,
+    lat: 0,
+    price: 0,
+    propertyId: 0,
+    amenities: '',
+    photo: '',
+    state: '',
+  };
   getModal() {
     throw new Error('Method not implemented.');
   }
-
 }
